b3eea2c4f6bd95440777a5925b206e4046e7e61c
Arash <arash8877@gmail.com>
2024-05-24 10:08:33 +0200

    add custom button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <ButtonGradient />
+      <div className="pt-[4.75rem] lg:pt-[5.25] overflow-hidden">
+        <Button className="mt-10" href="#login">something</Button>
+      </div>
     </>
   );
 };
